b248ab791c1783cf4427bab221cb3a89874d0e35
heriqueMH <mh88533@gmail.com>
2024-05-07 17:26:33 -0300

    first commit

---
+++ src/main/java/br/mackenzie/ProejtoN2/ProejtoN2Application.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ProejtoN2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProejtoN2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProejtoN2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/mackenzie/ProejtoN2/ProejtoN2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ProejtoN2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProejtoN2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
